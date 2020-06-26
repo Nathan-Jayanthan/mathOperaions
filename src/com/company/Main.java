d840d77feeba36d04f3587782e98dee8ee00f30b
nathan-jayanthan <nathan.jayanthan@yahoo.com>
2020-06-25 18:32:38 -0700

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int tuna = 5;
+        int bass =18;
+
+        System.out.println(++tuna);
+        //tuna++ will print 5 but change the variable to 6
+        //++tuna will first change the variable to 6 than also print 6
+    }
+}
